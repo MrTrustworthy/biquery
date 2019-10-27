883b4533815b27d50397873783c309a433f32ab1
MrTrustworthy <tinywritingmakesmailhardtoread@gmail.com>
2019-10-27 11:32:47 +0100

    implemented basic engine for executing models

--- app/biquery/builder.py
+++ app/biquery/builder.py
@@ -1,10 +1,17 @@
+from typing import Dict
+
 from biquery.metric_model import MetricModel
 from biquery.driver import Driver
 
 
 class QueryBuilder:
 
+    def __init__(self):
+        self.query_cache: Dict[str, str] = {}  # mainly used as introspection cache for testing right now
+
     def build_query(self, model: MetricModel) -> str:
         agg = model.agg.value.format(column=model.field.column_name)
         table = Driver.escaped_table_id(model.field.table)
-        return f"SELECT {agg} FROM {table}"
+        query = f"SELECT {agg} FROM {table}"
+        self.query_cache[model.name] = query
+        return query
--- app/tests/test_model.py
+++ app/tests/test_model.py
@@ -1,34 +1,42 @@
-from biquery.builder import QueryBuilder
-from biquery.driver import Driver
-from biquery.metric_model import MetricModel, Field, Aggregation
+from biquery.engine import Engine
 from tests.conftest import PROJECT
 
 
 def test_simple_count_model():
-    driver = Driver(PROJECT)
-    table = driver.get_table_by_name("test", "orders")
-    table_esc = Driver.escaped_table_id(table)
-    field = Field(table, "id")
-    model = MetricModel("test_simple_count", field, Aggregation.COUNT)
-    builder = QueryBuilder()
-    query = builder.build_query(model)
-    assert query == f"SELECT COUNT(id) FROM {table_esc}"
-    result = driver.run_query(query)
+    model_name = "test_simple_count_model"
+    model_conf = {
+        "name": model_name,
+        "field": {
+            "dataset": "test",
+            "table": "orders",
+            "column": "id"
+        },
+        "aggregation": "count"
+    }
+    engine = Engine(PROJECT)
+    engine.load_model_conf(model_conf)
+    result = engine.run_model(model_name)
+    query = engine.builder.query_cache[model_name]
+    assert query == f"SELECT COUNT(id) FROM `{PROJECT}.test.orders`"
     assert result == 1000
 
 
-def test_join_sum_model():
-    driver = Driver(PROJECT)
-    order_table = driver.get_table_by_name("test", "orders")
-    order_table_esc = Driver.escaped_table_id(order_table)
-    product_table = driver.get_table_by_name("test", "products")
-    product_table_esc = Driver.escaped_table_id(product_table)
+def test_1to1_reversed_join_sum_model():
+    model_name = "test_1to1_reversed_join_sum_model"
+    model_conf = {
+        "name": model_name,
+        "field": {
+            "dataset": "test",
+            "table": "products",
+            "column": "price"
+        },
+        "aggregation": "sum"
+    }
+    engine = Engine(PROJECT)
+    engine.load_model_conf(model_conf)
+    result = engine.run_model(model_name)
+    query = engine.builder.query_cache[model_name]
 
-    field = Field(order_table, "price")
-    model = MetricModel("test_simple_sum", field, Aggregation.SUM)
-    builder = QueryBuilder()
-    query = builder.build_query(model)
+    assert query == "select sum(price) from `nullpointer-184019.test.orders` left join `nullpointer-184019.test.products` on `nullpointer-184019.test.products`.id = `nullpointer-184019.test.orders`.product_id"
+    assert int(result) == 241531
 
-    assert query == f"SELECT SUM(price) FROM {order_table_esc} LEFT JOIN {product_table_esc} ON {product_table_esc}.id = {order_table_esc}.product_id"
-    result = driver.run_query(query)
-    assert result == 1000
